2e669feeb7805816ab5ce4dddbe75f9a654edc32
narvis2 <narvis2@naver.com>
2022-08-13 18:23:07 +0900

    Navigation Stack 을 이용한 화면 전환 기능 추가
    ## Navigation Stack 을 이용한 화면 전환 기능 추가
    - navigation 사용 시 인자로 navigation 과 route 를 가져올 수 있음
    > navigation.navigate()
    >> - 화면 전환 시 새로 이동할 화면이 현재 화면과 같으면 새로운 화면을 쌓지 않고 파라미터만 변경합니다.
    >> - native stack navigate 외  다른 navigate 에도 존재합니다.
    > navigation.push()
    >> - 화면 전환 시 새로운 화면이 계속 Stack에 쌓인다.
    >> - native stack navigate 에서만 사용 가능합니다.
    > route : 전환시에 데이터를 주고 받을 때 사용
    >> - reoute.key : navigate로 생성될 때 고유 key 값이 자동으로 생성됨.
    >> - reoute.name : 화면의 이름, App 컴포넌트에서 native stack navigate 를 설정할 때 지정한 이름
    >> - reoute.params: 화면 전환 시 지정한 라우트 파라미터(내가 전달한 데이터의 내용이 포함되어 있음.)
    > navigation.pop() : 뒤로가기
    > navigation.popToTop(): 가장 첫 번째 화면으로 이동

--- App.js
+++ App.js
@@ -3,11 +3,21 @@
  * https://github.com/facebook/react-native
  */
 
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./sreens/HomeScreen";
+import DetailScreen from "./sreens/DetailScreen";
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>
+  return (<NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Detail" component={DetailScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>);
 }
 
 export default App;
