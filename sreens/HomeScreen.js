215952e3a5dc436c486721564e46c592e12cf240
narvis2 <narvis2@naver.com>
2022-08-13 19:39:46 +0900

    헤더 변경 기능 추가
    ## 헤더 번경 기능 추가
    - 최상위 Component 에서 헤더에 원하는 Title 지정하는 코드 추가
    ``` javaScript
    <Stack.Screen
          name="Home"
          component={HomeScreen}
          options={{
            title: "홈",
            headerStyle: {
                backgroundColor: "#29b6f6",
            },
            // 헤더의 텍스트, 버튼들 색상
            headerTintColor: "#ffffff",
            // 타이틀 텍스트의 스타일
            headerTitleStyle: {
               fontWeight: "bold",
               fontSize: 20,
            },
          }}
        />
    ```
    - 각 Component에서 원하는 Header 지정하는 코드, useEffect Hook 을 사용하여 Component 가 보일 때 Header에 지정
    ``` javaScript
    useEffect(() => {
        navigation.setOptions({
          title: `상세 정보 - ${route.params.id}`,
        });
    }, [navigation]);
    ```

--- sreens/HomeScreen.js
+++ sreens/HomeScreen.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Button } from "react-native";
 
-function HomeScreen({navigation}) {
-    return (
-        <View>
-            <Button title="Detail 1 열기" onPress={() => navigation.push('Detail', {id: 1})} />
-            <Button title="Detail 2 열기" onPress={() => navigation.push('Detail', {id: 2})} />
-            <Button title="Detail 3 열기" onPress={() => navigation.push('Detail', {id: 3})} />
-        </View>
-    );
+function HomeScreen({ navigation }) {
+  // useEffect Hook 을 사용하여 컴포넌트가 화면에 나타났을 때 navigation.setOptions 를 사용하여 헤더의 제목을 바꿈
+  useEffect(() => {
+    navigation.setOptions({
+      title: "홈",
+    });
+  }, [navigation]);
+  return (
+    <View>
+      <Button
+        title="Detail 1 열기"
+        onPress={() => navigation.push("Detail", { id: 1 })}
+      />
+      <Button
+        title="Detail 2 열기"
+        onPress={() => navigation.push("Detail", { id: 2 })}
+      />
+      <Button
+        title="Detail 3 열기"
+        onPress={() => navigation.push("Detail", { id: 3 })}
+      />
+      <Button
+        title="Headless 열기"
+        onPress={() => navigation.push("Headerless")}
+      />
+    </View>
+  );
 }
 
 export default HomeScreen;
