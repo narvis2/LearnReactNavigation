97705ca9990220ce3e86452b21791dd6ae4296dc
narvis2 <narvis2@naver.com>
2022-08-13 17:19:04 +0900

    프로젝트 초기 구성
    ## 프로젝트 초기 구성
    - react-navigation 의존성 추가
    ``` Terminal
    yarn add @react-navigation/native
    ```
    - react-navigation-stack 의존성 추가
    ``` Terminal
    yarn add @react-navigation/native-stack
    ```

---
+++ App.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App() {
+  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>
+}
+
+export default App;
