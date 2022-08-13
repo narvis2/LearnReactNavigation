cefb13b8ab56c2ecc6254283ae3fd936ce0d9de1
narvis2 <narvis2@naver.com>
2022-08-13 20:25:05 +0900

    Drawer 기능 추가
    ## Drawer 기능 추가
    - Drawer Navigator Dependency 추가
    ``` Terminal
    $ yarn add @react-navigation/drawer-react-native-gesture-handler react-native-reanimated
    ```
    - drawer-react-native-gesture : 드로어 내비게이터에서 사용자 제스처를 인식하기 위하여 내부적으로 사용하는 라이브러리
    - react-native-reanimated : 리액트 네이티브에 내장된 애니메이션 효과 기능보다 더욱 개선된 성능으로 애니메이션 효과를 구현해주는 라이브러리
    - Drawer Navigator 는 navigation.push, navigation.pop 같은 기능들은 호환되지 않음
    - Drawer Navigator
    ``` javaScript
    import { NavigationContainer } from "@react-navigation/native";
    import { createDrawerNavigator } from "@react-navigation/drawer";
    import { SafeAreaView } from "react-native-safe-area-context";
    <Drawer.Navigator
    backBehavior="history"
            drawerContent={({ navigation }) => (
              <SafeAreaView>
                <Text>A Custom Drawer</Text>
                <Button
                  onPress={() => navigation.closeDrawer()}
                  title="Drawer 닫기"
                />
              </SafeAreaView>
            )}
            screenOptions={{
              drawerActiveBackgroundColor: "#fb8c00", // 활성화된 항복의 배경색 지정
              drawerActiveTintColor: "white", // 활성화된 항목의 텍스트 색상 지정
            }}
    >
    </Drawer.Navigator>
    ```
    > drawerContent : Drawer Component 가 나타나는 영역에 아예 다른 UI를 보여주고 싶을 때 사용
    >> 아이폰도 제공하는 경우 SafeAreaView 를 필수로 사용하여야함 그렇지 않으면 드로어의 상단부 영역이 StatusBar 영역과 겹치게 됨.
    
    > navigation.closeDrawer : 현재 보여지는 드로어를 닫아줌
    > navigation.navigate : 다른 화면으로 전환
    > navigation.openDrawer : 드로어 열기
    > navigation.goBack : 현재 보여지는 화면(Fragment) 닫기

--- sreens/HomeScreen.js
+++ sreens/HomeScreen.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { View, Button } from "react-native";
 
+/**
+ * 드로어 내비게이터에서 navigation.push, navigation.pop 같은 기능들은 호환되지 않음
+ */
 function HomeScreen({ navigation }) {
   // useEffect Hook 을 사용하여 컴포넌트가 화면에 나타났을 때 navigation.setOptions 를 사용하여 헤더의 제목을 바꿈
   useEffect(() => {
